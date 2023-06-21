8d1453ea533989cb532e9380b7dc4010492fe3e0
youssef-mahersi <126518742+youssef-mahersi@users.noreply.github.com>
2023-06-21 08:56:36 +0200

    Update index.js
    navbar

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Authenticate from './pages/Authenticate';
 import Home from './pages/Home';
-import { Provider } from 'react-redux'
-import store from './store'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const root = document.getElementById('root');
+render(
   <Provider store={store}>
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Authenticate />}/>
-          <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-   </Provider>
+    <React.StrictMode>
+      <>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a  className="flex items-center">
+              <img src="https://i.ibb.co/BPvdFzP/logo.png" className="h-8 mr-3" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Just Chat</span>
+            </a>
+          </div>
+        </nav>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Authenticate />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </React.StrictMode>
+  </Provider>,
+  root
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
